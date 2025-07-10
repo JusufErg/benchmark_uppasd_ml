0fd0bb9ce4258d90ce32ae1e28ceee7203962060
JusufErg <151943653+JusufErg@users.noreply.github.com>
2025-07-10 14:23:46 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+# Some names and paths that is useful later on, these are imported to files for Pytorch optimization. 
+
+simid = "bccFe100"
+folder_name = "bccFe"
+base_path = f"../uppasd_runs/{folder_name}"
+
