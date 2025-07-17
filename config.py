4b7757ef5c8f246a371956a0b8e11e9b746e4848
JusufErg <151943653+JusufErg@users.noreply.github.com>
2025-07-17 12:48:46 +0200

    Delete config.py

--- config.py
+++
@@ -1,6 +0,0 @@
-# Some names and paths that is useful later on, these are imported to files for Pytorch optimization. 
-
-simid = "bccFe100"
-folder_name = "bccFe"
-base_path = f"../uppasd_runs/{folder_name}"
-
